e79600777ed2b33f51e22259af87224393fc9c58
CarlosArias07 <carlos_biohazard@hotmail.es>
2017-11-27 10:47:35 -0600

    Section 2- Part 7

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,9 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            var customer = new Customer(1, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
